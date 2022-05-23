c8cf03b2a7758cd1dfad961cca2b5fc14870210b
Andrey Bozhkov <kovbozh@gmail.com>
2022-05-23 19:56:50 +0300

    edit views.py

--- lesson_2/views.py
+++ lesson_2/views.py
@@ -1,13 +1,12 @@
+# Модуль, содержащий контроллеры веб-приложения
 from dr0n_framework.templator import render
 
 
 class Index:
-    def __call__(self):
-        # Вызов a() эквивалентен вызову a.__call__().
+    def __call__(self, request):
         return '200 OK', render('index.html')
 
 
 class About:
-    def __call__(self):
-        # Вызов a() эквивалентен вызову a.__call__().
+    def __call__(self, request):
         return '200 OK', render('about.html')
