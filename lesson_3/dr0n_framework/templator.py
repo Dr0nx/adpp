b432790c54a86c0c7ec0a9236f9bd11ca3e72dde
Andrey Bozhkov <kovbozh@gmail.com>
2022-05-26 01:03:18 +0300

    Update templator.py

--- lesson_3/dr0n_framework/templator.py
+++ lesson_3/dr0n_framework/templator.py
@@ -2,13 +2,9 @@ from jinja2 import FileSystemLoader
 from jinja2.environment import Environment
 
 
-def render(template_name, folder='templates', **kwargs):
-    """
-    :param template_name: имя шаблона
-    :param kwargs: параметры для передачи в шаблон
-    :return:
-    """
+def render(template_name, folder='templates', static_url='/static/', **kwargs):
     environ = Environment()
     environ.loader = FileSystemLoader(folder)
+    environ.globals['static'] = static_url
     template = environ.get_template(template_name)
     return template.render(**kwargs)
