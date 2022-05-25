c68a5db9f76148d28fb94d4fefdfc73623e123b5
Andrey Bozhkov <kovbozh@gmail.com>
2022-05-26 01:03:05 +0300

    Update run.py

--- lesson_3/run.py
+++ lesson_3/run.py
@@ -1,9 +1,10 @@
 from wsgiref.simple_server import make_server
 from dr0n_framework.main import Framework
 from urls import routes
+from components import settings
 
 # Создаем объект WSGI-приложения
-application = Framework(routes)
+application = Framework(settings, routes)
 
 with make_server('', 8000, application) as httpd:
     print("Запуск на порту 8000...")
