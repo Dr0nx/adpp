f634766d3fd6354dd10a11a45a9fe8c31559c665
Andrey Bozhkov <39522099+Dr0nx@users.noreply.github.com>
2022-05-31 11:59:24 +0300

    Delete lesson_4 directory

--- lesson_4/dr0n_framework/templator.py
+++
@@ -1,9 +0,0 @@
-from jinja2 import FileSystemLoader
-from jinja2.environment import Environment
-
-
-def render(template_name, folder='templates', **kwargs):
-    env = Environment()
-    env.loader = FileSystemLoader(folder)
-    template = env.get_template(template_name)
-    return template.render(**kwargs)
