38df8f749bf9f0cd4eb0d6ac6243069d4a6b514f
Andrey Bozhkov <kovbozh@gmail.com>
2022-06-01 18:49:42 +0300

    Update models.py

--- lesson_5/components/models.py
+++ lesson_5/components/models.py
@@ -3,12 +3,15 @@ import quopri
 
 # Класс-Абстрактный пользователь
 class User:
-    pass
+    def __init__(self, name):
+        self.name = name
 
 
 # Класс-Студент
 class Student(User):
-    pass
+    def __init__(self, name):
+        self.courses = []
+        super().__init__(name)
 
 
 # Класс-Фабрика пользователей
@@ -17,9 +20,10 @@ class UserFactory:
         'student': Student,
     }
 
+    # порождающий паттерн Фабричный метод
     @classmethod
-    def create_factory(cls, type_):
-        return cls.types[type_]()
+    def create(cls, type_, name):
+        return cls.types[type_](name)
 
 
 # Класс-Курс
@@ -29,6 +33,15 @@ class Course:
         self.name = name
         self.category = category
         self.category.courses.append(self)
+        self.students = []
+        super().__init__()
+
+    def __getitem__(self, item):
+        return self.students[item]
+
+    def add_student(self, student: Student):
+        self.students.append(student)
+        student.courses.append(self)
 
 
 # Класс-Интерактивный курс
@@ -48,8 +61,9 @@ class CourseFactory:
         'record': RecordCourse
     }
 
+    # порождающий паттерн Фабричный метод
     @classmethod
-    def create_factory(cls, type_, name, category):
+    def create(cls, type_, name, category):
         return cls.types[type_](name, category)
 
 
@@ -74,14 +88,13 @@ class Category:
 # Класс-Основной интерфейс проекта
 class Engine:
     def __init__(self):
-        self.teachers = []
         self.students = []
         self.courses = []
         self.categories = []
 
     @staticmethod
-    def create_user(type_):
-        return UserFactory.create_factory(type_)
+    def create_user(type_, name):
+        return UserFactory.create(type_, name)
 
     @staticmethod
     def create_category(name, category=None):
@@ -89,20 +102,24 @@ class Engine:
 
     def find_category_by_id(self, id):
         for item in self.categories:
-            print('item', item.id)
             if item.id == id:
                 return item
         raise Exception(f'Нет категории с id = {id}')
 
     @staticmethod
     def create_course(type_, name, category):
-        return CourseFactory.create_factory(type_, name, category)
+        return CourseFactory.create(type_, name, category)
 
     def get_course(self, name):
         for item in self.courses:
             if item.name == name:
                 return item
         return None
+
+    def get_student(self, name):
+        for item in self.students:
+            if item.name == name:
+                return item
 
     @staticmethod
     def decode_value(val):
