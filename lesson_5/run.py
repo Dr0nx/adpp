eec1dbe1fa6e0439b230764e69ee7ae562d150e8
Andrey Bozhkov <kovbozh@gmail.com>
2022-06-01 18:50:00 +0300

    Update run.py

--- lesson_5/run.py
+++ lesson_5/run.py
@@ -7,5 +7,5 @@ from views import routes
 application = Framework(routes)
 
 with make_server('', 8000, application) as httpd:
-    print("Запуск на порту 8000...")
+    print("Запускаю на порту 8000...")
     httpd.serve_forever()
